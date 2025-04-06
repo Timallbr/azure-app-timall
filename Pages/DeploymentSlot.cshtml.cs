3e73c2d5de0a53f913ac946f55cd8d78e9238576
Timallbr <timallbr@gmail.com>
2025-04-06 14:15:21 -0600

    Add staging branch for setting up deployment slot

---
+++ Pages/DeploymentSlot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_timall.Pages
+{
+    public class DeploymentSlotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
